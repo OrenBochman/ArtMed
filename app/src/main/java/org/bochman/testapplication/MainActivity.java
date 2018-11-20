6cca64d7b3df55144762c3e113f7d69e79d3a3a9
OrenBochman <OrenBochman@gmail.com>
2018-11-21 01:39:42 +0200

    added comments and extracted variables.

--- app/src/main/java/org/bochman/testapplication/MainActivity.java
+++ app/src/main/java/org/bochman/testapplication/MainActivity.java
@@ -9,23 +9,28 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //the data
     private DataPoint data[];
+    //the graph control
+    private GraphView graph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        generateData();
+
+        graph =  findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(data);
+        graph.addSeries(series);
+    }
+
+    private void generateData() {
         data = new DataPoint[200];
         int i;
         for(i=0;i<200;i++){
             data[i]=new DataPoint((double)i,((double)i) / 2);
         }
-
-        GraphView graph =  findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(data);
-
-        graph.addSeries(series);
     }
 }
